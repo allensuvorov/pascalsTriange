5aaf1e7fd4df6c9b0d5fe18329bdfaea463831f2
Allen <allenvs@mail.ru>
2020-08-29 22:07:14 +0300

    added problem description

---
+++ app.js
@@ -0,0 +1,17 @@
+// Input: 5
+// Output:
+// [
+//      [1],
+//     [1,1],
+//    [1,2,1],
+//   [1,3,3,1],
+//  [1,4,6,4,1]
+// ]
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    
+};
