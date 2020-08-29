1778682a6747d2e8c7ee88e9a4daf19d10417c04
Allen <allenvs@mail.ru>
2020-08-30 00:03:20 +0300

    solution

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+
+let generate = function(numRows) {
+    let triangle = [];
+    for (let row = 0; row < numRows; row ++) {
+        // add empty row
+        triangle[row] = []; 
+        // set first and last entry to 1
+        triangle[row][0] = 1;
+        triangle[row][row] = 1;
+        // fill in the rest
+        for (let j = 1; j < row; j ++) {
+            triangle[row][j] = triangle[row-1][j-1] + triangle[row-1][j];
+        }
+    }
+    return triangle;
+};
+
+console.log(generate(5));
+
 // Input: 5
 // Output:
 // [
@@ -7,11 +30,3 @@
 //   [1,3,3,1],
 //  [1,4,6,4,1]
 // ]
-
-/**
- * @param {number} numRows
- * @return {number[][]}
- */
-var generate = function(numRows) {
-    
-};
